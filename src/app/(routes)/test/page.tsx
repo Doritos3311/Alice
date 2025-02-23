fde381763229c344b3f55ce7c97f99fbf4c34925
Doritos3311 <dorian.santamaria120@gmail.com>
2025-02-23 11:21:17 -0500

    Arreglo Ruta Test

--- src/app/(routes)/test/page.tsx
+++ src/app/(routes)/test/page.tsx
@@ -6,5 +6,7 @@ import Test from "@/components/ChatPanel/ChatPanel";
 export default function ComponenteTest() {
   return <Test isIAOpen={false} setIsIAOpen={function (isOpen: boolean): void {
     throw new Error("Function not implemented.");
+  } } setActiveTab={function (tab: string): void {
+    throw new Error("Function not implemented.");
   } } />;
 }
